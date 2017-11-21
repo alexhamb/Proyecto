efb27972bc8a9b3128bc19e1e9b8719ea5c9f764
“Alexhamb” <alex6_10@hotmail.com>
2017-11-21 11:26:20 -0500

    splash y login

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.asus.myapplication;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView opciones;
+    private String[] opc;
+    private ArrayAdapter adapter;
+    private Intent i;
+    private Resources res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opcionesprin);
+        res = this.getResources();
+        opciones = (ListView) findViewById(R.id.opc_prin);
+        opc = getResources().getStringArray(R.array.Menu_P);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, opc);
+        opciones.setAdapter(adapter);
+        opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent i = new Intent(MainActivity.this, Vp.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        i = new Intent(MainActivity.this, Crear_paciente.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/asus/myapplication/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.asus.myapplication;
+
+//CODE BY Humayun Kabir
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerTread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerTread.start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
